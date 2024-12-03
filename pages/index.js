ca163b974eda79cff19a877655718cc704443435
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-12-03 22:30:46 +0000

    feat: add font family

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ function Home() {
     const contentStyle = {
         display: 'flex',
         alignItems: 'center',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        fontFamily: '"Gill Sans", sans-serif',
+        padding: '2rem'
     }
     return <div style={contentStyle}> 
-        <h1>"I love you so much, my love, I could become Sonic and run around the world just to be next to you... though I might need a few golden rings to keep my energy up!" 💍💙</h1>
+        <h1>"My love, I could become Sonic and run around the world just to be next to you... though I might need a ring to keep my energy up!" 💍💙</h1>
         <img width={500} src="https://static.tildacdn.com/tild3533-6230-4835-b238-383765666664/sonic-sega.gif"></img>
     </div>
 };
