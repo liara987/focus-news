ad3d6c39355fc7cdab7e43e91d7dcea71c5bc3b8
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-12-03 22:21:56 +0000

    feat: update style

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {    
-    return <h1>"I love you so much, my love, I could become Sonic and run around the world just to be next to you... though I might need a few golden rings to keep my energy up!" 💍💙</h1>
+    const contentStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column'
+    }
+    return <div style={contentStyle}> 
+        <h1>"I love you so much, my love, I could become Sonic and run around the world just to be next to you... though I might need a few golden rings to keep my energy up!" 💍💙</h1>
+        <img width={500} src="https://static.tildacdn.com/tild3533-6230-4835-b238-383765666664/sonic-sega.gif"></img>
+    </div>
 };
 
 export default Home;
