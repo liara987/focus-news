c707b6e9b632a1b4b0d4cf6d7253ea19eeaf9762
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2025-02-23 03:43:44 +0000

    install jest

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,7 @@ function Home() {
         padding: '2rem'
     }
     return <div style={contentStyle}> 
-        <h1>"My love, I could become Sonic and run around the world just to be next to you... though I might need a ring to keep my energy up!" 💍💙</h1>
-        <img width={500} src="https://static.tildacdn.com/tild3533-6230-4835-b238-383765666664/sonic-sega.gif"></img>
+        
     </div>
 };
 
