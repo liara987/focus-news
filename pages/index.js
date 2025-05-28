2ad2cd4f064ce6117b92ed7f722645a706d978a7
liara987 <987.liara@gmail.com>
2025-05-27 22:24:31 -0300

    feat: add `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-function Home() {    
-    const contentStyle = {
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-        fontFamily: '"Gill Sans", sans-serif',
-        padding: '2rem'
-    }
-    return <div style={contentStyle}> 
-        
-    </div>
-};
+function Home() {
+  const contentStyle = {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    fontFamily: '"Gill Sans", sans-serif',
+    padding: "2rem",
+  };
+  return <div style={contentStyle}></div>;
+}
 
 export default Home;
