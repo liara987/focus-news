fe924db3d971a6e13466e0cb235705755171ad05
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2026-08-31 07:48:04 +0000

    cria `database.js` e `.env`

---
+++ infra/database.js
@@ -0,0 +1,21 @@
+import {Client} from 'pg'
+
+async function  query (queryObject) {
+    console.log('VARIAVEL QUE NÃO EXITE MAIS',process.env.POSTGRES_DATABASE)
+    const client = new Client({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD
+    })
+    await client.connect()
+    const result = await client.query(queryObject)
+    await client.end()
+    return result
+}
+
+
+export default {
+    query: query
+}
